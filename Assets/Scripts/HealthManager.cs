45a335900823117bb85b4b224cc7bbd642de08fe
Amadeusz <dluzniewskiamadeusz@gmail.com>
2024-06-12 15:38:22 +0200

    HealthManager class works with enemy health

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -8,18 +8,25 @@ public class HealthManager : MonoBehaviour
 {
     public Image healthBar;
     public float healthAmount = 100f;
+    public Animator animator;
 
     private void Update()
     {
         if (healthAmount <= 0)
         {
-            Application.LoadLevel(Application.loadedLevel);
+            Die();
         }
     }
+
     public void TakeDamage(float damage)
     {
         healthAmount -= damage;
         healthBar.fillAmount = healthAmount / 100f;
+
+        if(healthAmount <= 0)
+        {
+            Die();
+        }
     }
 
     public void Heal(float heal)
@@ -28,5 +35,17 @@ public class HealthManager : MonoBehaviour
         healthAmount = Mathf.Clamp(healthAmount, 0, 100);
 
         healthBar.fillAmount = healthAmount / 100f;
+    }
+
+    private void Die()
+    {      
+        animator.SetTrigger("Die");
+        StartCoroutine(RemoveAfterDelay(10f));
+    }
+
+    private IEnumerator RemoveAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(gameObject);
     }
 }
